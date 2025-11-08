ce5e4fd420a30414bfdee8943caa015b28fc2a85
SAMEER ALAM <sameeralam3127@gmail.com>
2025-11-08 23:22:19 +0530

    Order Priority

--- data_inspect.py
+++ data_inspect.py
@@ -6,7 +6,6 @@ file_path = "Super Store Sales/SuperStoreUS-2015.xlsx"
 # Read Excel file
 df = pd.read_excel(file_path)
 
-
 # Summary statistics
 print("\n📊 Summary Statistics:")
 print(df.describe(include='all').T)
